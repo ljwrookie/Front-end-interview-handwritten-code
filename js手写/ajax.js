3e2be2dc80935d4d5d5b3f26f8fa6b977000344c
1455387909@qq.com <1455387909@qq.com>
2022-04-24 11:02:34 +0800

    树与数组互转

--- js手写/ajax.js
+++ js手写/ajax.js
@@ -1,20 +1,36 @@
-//实现ajax请求
-function ajax(url, data, callback) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			callback(xhr.responseText);
+function myAjax(url, method, data) {
+	let promise = new Promise((resolve, reject) => {
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4) {
+				if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+					resolve(xhr.response);
+				} else {
+					reject(new Error("error"));
+				}
+			}
+		};
+		if (method.toUpperCase() === "GET") {
+			let paramslist = [];
+			for (key in data) {
+				paramslist.push(key + "=" + data[key]);
+			}
+			//根据get请求方法对url进行拼接
+			let params = paramslist.join("&");
+			url = url + "?" + params;
+			xhr.open("get", url, false);
+			//使用get请求将内容连接在url后面
+			xhr.send();
 		}
-	};
-	xhr.send(data);
-}
-//使用promise封装ajax请求
-function ajaxPromise(url, data) {
-	return new Promise(function (resolve, reject) {
-		ajax(url, data, function (res) {
-			resolve(res);
-		});
+		if (method.toUpperCase() === "POST") {
+			xhr.open("post", url, false);
+			xhr.setRequestHeader(
+				"Content-Type",
+				"application/x-www-form-urlencoded;charset=utf-8"
+			);
+			xhr.send(data);
+			//使用post请求时将内容放在send里面
+		}
 	});
+	return promise;
 }
